ee55ee200be95e51b9124c0e6197fa4546462899
hans0 <kiessler.hans@gmail.com>
2021-04-26 16:33:06 -0400

    Done with Login styling

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,20 +3,27 @@ import React from 'react';
 export default function Login() {
 
   const handleSubmit = e => {
+    e.preventDefault();
     console.log('submitted');
   }
 
   return(
     <div className="login">
       <h1>Login</h1>
-      <form>
-        <label>Username:</label>
-        <input type="text"></input>
-        <label>Password:</label>
-        <input type="password"></input>
+      <form onSubmit={handleSubmit}>
+        <>
+          <label>Username:</label>
+          <input type="text"></input>
+        </>
+        <>
+          <label>Password:</label>
+          <input type="password"></input>
+        </>
+
         <button>Login</button>
+
       </form>
-      Don't have an account? Register
+      <p>Don't have an account? <a href="#">Register</a></p>
     </div>
   );
 }
