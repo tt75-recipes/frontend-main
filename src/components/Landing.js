bfd2584d0ac6159e260d9ab63228847a9a15842b
hans0 <kiessler.hans@gmail.com>
2021-04-26 13:05:21 -0400

    Started Landing page

---
+++ src/components/Landing.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+export default function Landing() {
+
+  return(
+    <>
+      <h1>Secret Family Recipes</h1>
+      
+      <a href='/signup'>Sign Up</a>
+      <a href='/login'>Login</a>
+    </>
+  );
+
+}
