edf89738ffc592a8dd7bde95e75cae0491219034
hans0 <kiessler.hans@gmail.com>
2021-04-27 15:29:34 -0400

    Added logo images, and fixed the Login page to spec

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,16 +1,22 @@
 import React from 'react';
 
+import { ReactComponent as Logo_Large } from '../images/logo_Large.svg'
 
 export default function Landing() {
 
   return(
-    <div className='landing'>
-      <h1>Secret Family Recipes</h1>
-      <div className='landing-links'>
-        <a href='/signup'>Sign Up</a>
-        <a href='/login'>Login</a>
+    <>
+      <div className='landing'>
+        {/* <h2 className='josefin'>Secret Family</h2><h1>Recipes</h1> */}
+        <div style={{height:'200px', width: '500px', margin: 'auto'}} >
+          <Logo_Large />
+        </div>
+        <div className='landing-links'>
+          <a href='/signup'>Sign Up</a>
+          <a href='/login'>Login</a>
+        </div>
       </div>
-    </div>
+    </>
   );
 
 }
