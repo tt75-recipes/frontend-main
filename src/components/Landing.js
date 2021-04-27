70db8283962167452403c58cbe91510cab8f09f8
Cory Young <teigrgwyn@gmail.com>
2021-04-27 11:22:41 -0500

    set cateogories from state

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,15 +1,22 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 export default function Landing() {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://tt75-recipes.herokuapp.com/api/categories')
+      .then(res => setCategories(res.data))
+      .catch(err => console.log(err))
+  }, [])
 
   return(
     <div className='landing'>
       <h1>Secret Family Recipes</h1>
-      <div className='landing-links'>
+      <nav className='landing-links'>
         <a href='/signup'>Sign Up</a>
         <a href='/login'>Login</a>
-      </div>
+      </nav>
     </div>
   );
 
