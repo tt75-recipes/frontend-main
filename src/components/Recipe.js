f5bb6616631ff825ae3490e94f6b0019494ea1db
mariusmoldovan <biris_21@yahoo.com>
2021-04-27 11:02:03 -0500

    update recipe.js

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -5,17 +5,17 @@ export default function Recipe() {
 
   return(
     <div className='recipe'>
-      <h1>Recipe Title</h1>
+      <h2>Recipe Title</h2>
       <hr />
         <button>Edit</button>
         <button>Delete</button>
         <div className='content-recipe'>
-            <h2>Ingredients</h2>
+            <h3>Ingredients</h3>
                 <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quia aspernatur repellat inventore excepturi quae maxime veniam mollitia in, voluptatum distinctio unde non? Labore, fuga at pariatur repudiandae magni et nam!</p>
                 <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quia aspernatur repellat inventore excepturi quae maxime veniam mollitia in, voluptatum distinctio unde non? Labore, fuga at pariatur repudiandae magni et nam!</p>
                 <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quia aspernatur repellat inventore excepturi quae maxime veniam mollitia in, voluptatum distinctio unde non? Labore, fuga at pariatur repudiandae magni et nam!</p>
             
-            <h2>Directions</h2>
+            <h3>Directions</h3>
                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad maiores, est, quas nihil eveniet ea illo neque consequatur quia inventore, voluptate eum laborum quaerat accusantium eos rerum saepe nesciunt iste.</p>
       
         </div>
