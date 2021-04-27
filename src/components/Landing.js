876c089e5c97d29078ca48f84f6592570775fd7b
Cory Young <teigrgwyn@gmail.com>
2021-04-27 14:54:06 -0500

    final display of categories/recipes

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,35 +1,76 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-//import '../styles/landing.css'; // we talked about styled-components, but nothing concrete yet -- using plain css for now, can easily change later
+import '../styles/landing.css';
 
 export default function Landing() {
   const [tokenExists, setTokenExists] = useState(false);
   const [categories, setCategories] = useState([]);
+  const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState('');
+  const [radio, setRadio] = useState('');
+
+  const filteredRecipes = recipes.filter(recipe => {
+    if (recipe.title.toLowerCase().includes(search.toLowerCase()))
+      return true;
+  })
 
   useEffect(() => {
     const token = window.localStorage.getItem('token');
     if (token) setTokenExists(true);
+    console.log(localStorage);
+
+    if (token) {
+      axios.get('https://tt75-recipes.herokuapp.com/api/categories')
+        .then(res => setCategories(res.data))
+        .catch(err => console.log(err))
+      axios.get('https://tt75-recipes.herokuapp.com/api/recipes')
+        .then(res => setRecipes(res.data))
+        .catch(err => console.log(err))
+    }
   }, [])
 
-  useEffect(() => {
-    axios.get('https://tt75-recipes.herokuapp.com/api/categories')
-      .then(res => setCategories(res.data))
-      .catch(err => console.log(err))
-  }, [tokenExists])
+  const handleSearch = e => setSearch({ ...search, [e.target.name]: e.target.value });
+  const handleRadio = e => setRadio({ ...radio, [e.target.name]: e.target.value });
 
-  // conditional landing page logic
-  //    logged in = display search bar & all the recipes
-  //    not logged in = only show register/login
-  if (tokenExists) {
+  if (!tokenExists) {
     return (
       <div className='landing'>
         <h1>Secret Family Recipes</h1>
         <div className='container-search-recipes'>
           <aside className='searchbar'>
-            placeholder
+            <input onChange={handleSearch}
+              name='search'
+              value={search}
+              placeholder='search'
+              spellCheck='false'
+              autoComplete='off'
+            />
+            <div onChange={handleRadio}>
+              {
+                categories.map(category => {
+                  return (
+                    <label>{category}
+                    <input type='radio' value={category} name='radio-categories' />
+                    </label>
+                  )
+                })
+              }
+            </div>
           </aside>
           <main className='recipelist'>
-            placeholder
+            {
+              filteredRecipes.map(recipe => {
+                return (
+                  <div className='recipe-card'>
+                    <h2>Title:</h2> <p>{recipe.title}</p>
+                    <h3>Source:</h3> <p>{recipe.source}</p>
+                    <h3>Instructions:</h3> <p>{recipe.instructions}</p>
+                    <h3>Ingredients:</h3> <p>{recipe.ingredients}</p>
+                    <h3>Category:</h3> <p>{recipe.category}</p>
+                  </div>
+                )
+              })
+            }
           </main>
         </div>
       </div>
