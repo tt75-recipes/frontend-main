92507ef721a2d7c52f26c058d6f27aacf92c3a6a
Chance Swartz <71352443+chance10113@users.noreply.github.com>
2021-04-29 18:04:50 -0500

    Fixed unnecessary axios call that was preventing deploy

--- src/components/NewRecipe.js
+++ src/components/NewRecipe.js
@@ -1,6 +1,5 @@
 import React, {useState, useEffect} from "react";
 import {useHistory} from "react-router-dom";
-import axios from "axios";
 import * as yup from "yup";
 import Schema from "./FormSchema";
 import {axiosWithAuth} from "../axiosWithAuth";
